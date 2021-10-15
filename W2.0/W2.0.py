61ca263185206ce94df13db35efe36b39a6583a8
Сергей <fanguffi88@gmail.com>
2021-10-15 10:27:00 +0300

    v.015

--- W2.0/W2.0.py
+++ W2.0/W2.0.py
@@ -5,7 +5,7 @@ colors = ['#a6cee3', '#1f78b4','#b2df8a', '#33a02c', '#fb9a99', '#e31a1c', '#fdb
 targetPattern = rootpath+'INPUT_CSV\\*.csv'
 listFiles = []
 listFiles = glob.glob(targetPattern)
-G = nx.MultiDiGraph()
+G = nx.DiGraph()
 for list in listFiles:
     with open(list) as csvfile:
         spamreader = csv.reader(csvfile)
@@ -13,18 +13,17 @@ for list in listFiles:
         i=0
         for row in spamreader:
             if row[7] != "0":
-                G.add_node(row[4], URL="https://etherscan.io/address/"+row[4], label=row[1], tooltip=row[4])
-                G.add_node(row[5], URL="https://etherscan.io/address/"+row[5], label=row[1], tooltip=row[4])
+                G.add_node(row[4], URL="https://etherscan.io/address/"+row[4], label=row[4][2:5]+'..'+row[4][-3:], tooltip=row[4])
+                G.add_node(row[5], URL="https://etherscan.io/address/"+row[5], label=row[1], tooltip=row[5])
                 G.add_edge(row[4],row[5], URL= "https://etherscan.io/tx/"+row[0], tooltip= row[3]+' | '+row[7], color = colors[i])
             else:
-                G.add_node(row[4], URL="https://etherscan.io/address/"+row[4], label=row[1], tooltip=row[4])
-                G.add_node(row[5], URL="https://etherscan.io/address/"+row[5], label=row[1], tooltip=row[4])
+                G.add_node(row[4], URL="https://etherscan.io/address/"+row[4], label=row[4][2:5]+'..'+row[4][-3:], tooltip=row[4])
+                G.add_node(row[5], URL="https://etherscan.io/address/"+row[5], label=row[1], tooltip=row[5])
                 G.add_edge(row[4],row[5], URL= "https://etherscan.io/tx/"+row[0], tooltip= row[3]+' | '+row[8], color = colors[i])
             if i==11: 
                 i = 0
             else:
                 i=i+1
-
 nx.drawing.nx_pydot.write_dot(G, rootpath+'multi_dot.dot')
 graph = nx.drawing.nx_pydot.to_pydot(G)
-graph.write_svg('output.svg', prog = 'circo', rankdir = "TB")
+graph.write_svg('output.svg', prog = 'circo')
